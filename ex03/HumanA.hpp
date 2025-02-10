4b7e380d5f718fe557ef6ae068d7896620bc24f2
Dominik Kaiser <dkaiser@student.42heilbronn.de>
2025-02-10 12:32:47 +0100

    Add HumanA and HumanB classes

--- ex03/HumanA.hpp
+++ ex03/HumanA.hpp
@@ -6,11 +6,24 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 11:59:28 by dkaiser           #+#    #+#             */
-/*   Updated: 2025/02/10 11:59:50 by dkaiser          ###   ########.fr       */
+/*   Updated: 2025/02/10 12:25:54 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HUMANA_H_
 #define HUMANA_H_
+
+#include "Weapon.hpp"
+#include <string>
+
+class HumanA
+{
+    public:
+        HumanA(const std::string &name, const Weapon &weapon);
+        void attack(void) const;
+    private:
+        Weapon weapon;
+        std::string name;
+};
 
 #endif
--- ex03/HumanB.hpp
+++ ex03/HumanB.hpp
@@ -6,11 +6,25 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 12:00:55 by dkaiser           #+#    #+#             */
-/*   Updated: 2025/02/10 12:00:58 by dkaiser          ###   ########.fr       */
+/*   Updated: 2025/02/10 12:29:43 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HUMANB_H_
 #define HUMANB_H_
+
+#include "Weapon.hpp"
+#include <string>
+
+class HumanB
+{
+    public:
+        HumanB(const std::string &name);
+        void attack(void) const;
+        void setWeapon(Weapon *weapon);
+    private:
+        Weapon* weapon;
+        std::string name;
+};
 
 #endif
