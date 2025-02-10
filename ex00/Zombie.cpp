dc032706847335c7e76b26492725939318f5f84e
Dominik Kaiser <dkaiser@student.42heilbronn.de>
2025-02-10 10:46:17 +0100

    Add and implement Zombie class

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,7 +6,23 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 10:27:11 by dkaiser           #+#    #+#             */
-/*   Updated: 2025/02/10 10:27:12 by dkaiser          ###   ########.fr       */
+/*   Updated: 2025/02/10 10:46:03 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+#include <iostream>
+
+Zombie::Zombie(const std::string &name)
+    :name(name)
+{}
+
+Zombie::~Zombie(void)
+{
+    std::cout << name << " died." << std::endl;
+}
+
+void Zombie::announce(void)
+{
+    std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,23 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 10:26:18 by dkaiser           #+#    #+#             */
-/*   Updated: 2025/02/10 10:26:19 by dkaiser          ###   ########.fr       */
+/*   Updated: 2025/02/10 10:38:18 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_H_
+#define ZOMBIE_H_
+
+#include <string>
+
+class Zombie
+{
+    public:
+        Zombie(const std::string &name);
+        ~Zombie(void);
+        void announce(void);
+    private:
+        std::string name;
+}
+
+#endif
