4b927759598b01c486063d09551303a936752956
Dominik Kaiser <dkaiser@student.42heilbronn.de>
2025-02-10 12:21:03 +0100

    Add and implement Weapon class

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -6,7 +6,21 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 11:59:21 by dkaiser           #+#    #+#             */
-/*   Updated: 2025/02/10 11:59:21 by dkaiser          ###   ########.fr       */
+/*   Updated: 2025/02/10 12:11:07 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+
+Weapon::Weapon(void)
+{}
+
+const std::string& Weapon::getType(void) const
+{
+    return this->type;
+}
+
+void Weapon::setType(const std::string &type)
+{
+    this->type = type;
+}
--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,11 +6,23 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 11:58:44 by dkaiser           #+#    #+#             */
-/*   Updated: 2025/02/10 11:59:10 by dkaiser          ###   ########.fr       */
+/*   Updated: 2025/02/10 12:09:57 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WEAPON_H_
 #define WEAPON_H_
+
+#include <string>
+
+class Weapon
+{
+    public: 
+        Weapon(void);
+        const std::string& getType(void) const;
+        void setType(const std::string &type);
+    private:
+        std::string type;
+};
 
 #endif
